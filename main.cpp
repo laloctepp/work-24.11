f06ba9598d6f5f476d93bfcf05debb4edf31b4fb
laloctepp <lbychkovskaya.omsk@gmail.com>
2025-11-24 13:25:10 +0300

    release functions2

--- main.cpp
+++ main.cpp
@@ -8,10 +8,20 @@ struct Planar {
 };
 
 Planar * make (std::istream& is);
-Planar * make(size_t pl);
+Planar * make(size_t id);
 void draw(Planar * pl);
 void free_planars(Planar ** pls, size_t k);
 Planar * mostLeft (Planar ** pls, size_t k);
+
+struct Point:Planar {
+  virtual int x() const;
+  virtual int y() const;
+  virtual int abs_sqr() const;
+  Point(int xx, int yy);
+  virtual ~Point() = default;
+  private:
+    int data[2];
+};
 
 int main()
 {
@@ -35,13 +45,16 @@ int main()
 }
 
 
-Planar * make(size_t pl) {
-  switch (pl)
-  {
+Planar * make(size_t id) {
+  Planar * r = nuulptr;
+  switch (id) {
+    case 0:
+      r = new Point(0,0);
+      break;
     default:
       throw std::logic_error("bad id");
   }
-  return nullptr;
+  return r;
 }
 
 void free_planars(Planar ** pls, size_t k) {
@@ -54,4 +67,21 @@ void draw(Planar * pl) {
   std::cout << pl->x();
   std::cout << " ";
   std::cout << pl->y() << "\n";
-} 
+}
+
+Point::Point(int xx, int yy):
+  Planar();
+  data = {xx, yy}
+{}
+
+int Point::x() const {
+  return data[0];
+}
+
+int Point::y() const {
+  return data[1];
+}
+
+int Point::abs_sqr() const {
+  return x() * x() + y() * y();
+}
