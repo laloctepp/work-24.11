0e49faa2dfe8de01066164deb3869bc327283241
laloctepp <lbychkovskaya.omsk@gmail.com>
2025-11-24 12:57:28 +0300

    base2

--- main.cpp
+++ main.cpp
@@ -8,12 +8,26 @@ struct Planar {
 };
 
 Planar * make (std::istream& is);
+Planar * make(size_t pl)
+void draw(Planar * pl);
+void free_planars(Planar ** pls, size_t k);
 Planar * mostLeft (Planar ** pls, size_t k);
 
 int main()
 {
   Planar * pls[10] = {};
   size_t k = 0;
+  for (size_t i = 0; i < 10; ++i) {
+    try {
+      pls[k] = make(i%2)
+    }
+    catch (...) {
+    free_planars(pls, k);
+    return 2;
+    }
+    k++;
+  }
+  draw(mostLeft(pls, k));
   //pls[k++] = make(std::cin);
   //Planar * left = mostLeft(pls, k);
   //draw(left)
