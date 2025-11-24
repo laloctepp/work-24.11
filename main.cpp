e2a30a5a490bf543f5bf5df3a478243092288571
laloctepp <lbychkovskaya.omsk@gmail.com>
2025-11-24 12:48:26 +0300

    base planar

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+#include <iostream>
+
+struct Planar {
+  virtual int x() const;
+  virtual int y() const;
+  virtual int abs_sqr() const = 0;
+  virtual ~Planar() = default;
+};
+
+Planar * make (std::istream& is);
+Planar * mostLeft (Planar ** pls, size_t k);
+
 int main()
 {
+  Planar * pls[10] = {};
+  size_t k = 0;
+  //pls[k++] = make(std::cin);
+  //Planar * left = mostLeft(pls, k);
+  //draw(left)
+  //free planars(psl, k);
 }
