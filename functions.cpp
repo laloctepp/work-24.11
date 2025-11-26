f64f85dd00887b661a7df87cee1e9e71f6207ec4
laloctepp <lbychkovskaya.omsk@gmail.com>
2025-11-26 12:30:16 +0300

    add 2make

--- functions.cpp
+++ functions.cpp
@@ -27,3 +27,18 @@ void draw(Planar * pl) {
   std::cout << pl->y() << "\n";
 }
 
+Planar * make (std::istream& is) {
+  char cmd[2] = {};
+  is >> cmd[0] >> cmd[1];
+  int data[4] = {};
+  if (cmd[0] == 'P' && cmd[1] == 'T')
+    if (is >> data[o] >> data[1]) {
+      return new Point(data[0], data[1]);
+    }
+  } else if (cmd[0] == 'V' && cmd[1] == 'T') {
+    if (is >> data[0] >> data[1] >> data[2] >> data[3]) {
+      return new Vector(Point(data[0],data[1]), Point(data[2], data[3]));
+    }
+  }
+  throw std::logic_error("bad cmd");
+}
